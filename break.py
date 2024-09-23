1851a1532729d7821e64e894f50b9fec8e55db43
BigBrainZuhu <102472376+BigBrainZuhu@users.noreply.github.com>
2024-09-23 18:04:48 +0600

    Create break.py

---
+++ break.py
@@ -0,0 +1,11 @@
+a=input("Enter a word:")
+for i in a:
+    if(i=='A'):
+        print("A is found.")
+        break
+    else:
+        print("A is not found.")
+
+def func(a,b):
+    return b*b
+print (func(5,2))
